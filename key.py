d1905e041046e54bfc0931a53cfbc0845f387ee0
Rattatwinko <89251490+ZockerKatze@users.noreply.github.com>
2025-02-14 20:48:50 +0100

    Create key.py
    this is for the main GUI!

---
+++ key.py
@@ -0,0 +1,7 @@
+# client file with API key!
+
+## if you dont have your API key yet run getapikey!
+
+from tgtg import TgtgClient
+
+client = TgtgClient(access_token="your key",refresh_token="your key",cookie="your key")
